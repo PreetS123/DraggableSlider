cb11e3f1e03f3842c3048ac975de31379308d3ca
aashishthakurcodes <j9183208@gmail.com>
2023-09-17 13:00:30 +0530

    infinite scrolling and auto play working properly

--- index.js
+++ index.js
@@ -47,11 +47,12 @@ const carouselItem = [
   ]
   
 const carousel= document.querySelector(".carousel");
+const wrapper= document.querySelector(".wrapper");
 const arrowBtn= document.querySelectorAll(".wrapper i");
 const firstCardWidth= carousel.querySelector(".card").offsetWidth;
 const carouselChildrens=[...carousel.children];
 
-let isDragging= false,startX,startScrollingLeft;
+let isDragging= false,startX,startScrollingLeft, timeoutId;
 
 //Get the number of cards that can fit into carousel at once
 let cardPerView= Math.floor(carousel.offsetWidth/firstCardWidth);
@@ -74,6 +75,14 @@ arrowBtn.forEach(btn=>{
     })
 })
 
+const autoPlay=()=>{
+  if(window.innerWidth<800) return; //Return if window is smaller than 800
+  //auto play the carousel after every 2500 sec.
+  timeoutId= setTimeout(()=>carousel.scrollLeft += firstCardWidth,1500)
+}
+
+autoPlay();
+
 const dragging=(e)=>{
     // console.log(e.pageX);
     if(!isDragging) return;
@@ -89,7 +98,34 @@ const dragStop=(e)=>{
     isDragging=false;
     carousel.classList.remove("dragging");
 }
+
+const infiniteScroll=()=>{
+  // if the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft<=0){
+      carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    setTimeout(() => {
+      carousel.classList.remove("no-transition");
+    }, 0);
+    console.log("left end");
+    }
+    else if(Math.ceil(carousel.scrollLeft)>=carousel.scrollWidth-carousel.offsetWidth){
+      carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    setTimeout(() => {
+      carousel.classList.remove("no-transition");
+    }, 0);
+    console.log("right end");
+    }
+    //clear existing timeout and start autoplay if mouse is not hovering 
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+
+}
    
    carousel.addEventListener("mousemove",dragging);
    carousel.addEventListener("mousedown",draggStart);
    document.addEventListener("mouseup",dragStop);
+   document.addEventListener("scroll",infiniteScroll);
+   wrapper.addEventListener("mouseenter",()=>clearTimeout(timeoutId));
+   wrapper.addEventListener("mouseleave",autoPlay);
